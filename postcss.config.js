77cccc5da0053fe1f65dbe429a255e22b328d473
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2019-05-12 11:38:06 -0400

    Initial css layout.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./*.html', './src/**/*.vue'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production'
+      ? [purgecss, require('cssnano')]
+      : [])
+  ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="w-screen h-screen flex flex-col overflow-hidden">
+    <nav class="flex-none flex items-center h-10 bg-blue-800 text-blue-100">
+      Top navigation
+    </nav>
+    <div class="flex-auto flex overflow-hidden">
+      <nav
+        class="flex-none flex flex-col items-center bg-blue-900 text-blue-100"
+      >
+        Side nav
+      </nav>
+      <div
+        class="blade-track flex-auto flex items-strech overflow-y-none overflow-x-scroll"
+      >
+        <div class="blade flex-none border-r border-blue-800 w-64">Blade 1</div>
+        <div class="blade flex-none border-r border-blue-800 w-64">Blade 2</div>
+        <div class="blade flex-none border-r border-blue-800 w-64">Blade 3</div>
+        <div class="blade flex-none border-r border-blue-800 w-64">Blade 4</div>
+        <div class="blade flex-none border-r border-blue-800 w-64">Blade 5</div>
+        <div class="blade flex-none border-r border-blue-800 w-64">Blade 6</div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {}
+</script>
